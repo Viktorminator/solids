722cc333551689eef141212614f2318b33f9cad0
Viktor Matushevskyi <viktorminator@gmail.com>
2017-02-17 00:17:16 +0200

    Article template author name

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,5 +2,6 @@
 @section('content')
     <h1 class="big">{{ $parent->pagetitle }}</h1>
     <h1>{{ $post->pagetitle }}</h1>
+    <p class="desc">{{ $post->published }}, {{ $post->author->author_name }}</p>
     {!!  $post->content !!}
 @stop
