485385980d5433c672ecbf47d638baee895a5e68
Viktor Matushevskyi <viktorminator@gmail.com>
2017-02-25 02:20:49 +0200

    News format localized

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -6,7 +6,7 @@
 </ul>
     @foreach($posts as $item)
         <h3><a href="{{ $item->alias }}" title="{{ $item->title }}">{{ $item->pagetitle }}</a></h3>
-        <p class="desc"> {{ \Carbon\Carbon::createFromFormat('U', $item->published)->formatLocalized('%d %B') }} </p>
+        <p class="desc"> {{ Carbon\Carbon::parse($item->published)->formatLocalized('%d %B') }} </p>
         <p class="mini">{{ $item->description }}</p>
     @endforeach
 @stop
